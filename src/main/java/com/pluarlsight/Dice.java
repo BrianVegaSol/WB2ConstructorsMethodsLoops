2c687fe2d74154f5a122d0d2b43c08776edb13ab
BrianVegaSol <brivegasol@gmail.com>
2024-10-05 10:09:46 -0700

    RollTheDice C l e a r e d
    Got the switch working
    
    Fixed an "issue" with the Dice object

--- src/main/java/com/pluarlsight/Dice.java
+++
@@ -1,9 +0,0 @@
-package com.pluarlsight;
-
-public class Dice {
-    //int randomNumber = (int)(Math.random() * maxValue) + minValue;
-public static int roll(){
-    return (int)(Math.random() * 6) + 1;
-}
-
-}
--- src/main/java/com/pluarlsight/RollTheDice.java
+++ src/main/java/com/pluarlsight/RollTheDice.java
@@ -10,18 +10,20 @@ public class RollTheDice {
         int count4Rolls = 0;
         int count6Rolls = 0;
         int count7Rolls = 0;
-
+        //Not necessary??? Used Dice.roll before and this was unused...
+        //Was because the roll() was static
         Dice dice = new Dice();
 
-        for (i = 1; i < 5; i++) {
-            roll1 = Dice.roll();
-            roll2 = Dice.roll();
+        for (i = 1; i < 101; i++) {
+            roll1 = dice.roll();
+            roll2 = dice.roll();
             result = roll1 + roll2;
             System.out.println("Round " + i + " \n\n" +
                     "Die 1: " + roll1 + "\n" +
                     "Die 2: " + roll2 + "\n" +
                     "Result: " + result);
             System.out.println("--------------------------");
+            //L e g a c y hahaaa
             /*if (roll1 == 2 || roll2 == 2) {
                 count2Rolls++;
             }
@@ -31,23 +33,10 @@ public class RollTheDice {
             if (Dice.roll() == 6) {
                 count6Rolls++;
             }*/
+
             //Never forget the breaks lol
             //switch (Dice.roll()){ Getting weird results
-            switch (roll1){
-                case 2:
-                    count2Rolls ++;
-                    break;
-                case 4:
-                    count4Rolls ++;
-                    break;
-                case 6:
-                    count6Rolls ++;
-                    break;
-                case 7:
-                    count7Rolls ++;
-                    break;
-            }
-            switch (roll2){
+            switch (result){
                 case 2:
                     count2Rolls ++;
                     break;
@@ -62,11 +51,16 @@ public class RollTheDice {
                     break;
             }
         }
-        //Tallys
-        //System.out.println("--------------------------");
+        //Result Tallies
         System.out.println("#2 Rolled: " + count2Rolls + " times");
         System.out.println("#4 Rolled: " + count4Rolls + " times");
         System.out.println("#6 Rolled: " + count6Rolls + " times");
         System.out.println("#7 Rolled: " + count7Rolls + " times");
+    }
+}
+class Dice {
+    //int randomNumber = (int)(Math.random() * maxValue) + minValue;
+    public int roll(){
+        return (int)(Math.random() * 6) + 1;
     }
 }
