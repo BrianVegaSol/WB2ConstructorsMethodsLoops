95ff1c6b45cd2ca1c514ac33d7b65d1a679557be
BrianVegaSol <brivegasol@gmail.com>
2024-10-05 08:17:38 -0700

    Ex 3 Bonus

--- src/main/java/com/pluarlsight/CellPhone.java
+++ src/main/java/com/pluarlsight/CellPhone.java
@@ -8,45 +8,51 @@ public class CellPhone {
     private String owner = "";
 
     //Empty constructor
-    public CellPhone(){
+    public CellPhone() {
     }
 
-    public int getSerialNumber(){
+    public int getSerialNumber() {
         return this.serialNumber;
     }
-    public void setSerialNumber(int serialNumber){
+
+    public void setSerialNumber(int serialNumber) {
         this.serialNumber = serialNumber;
     }
 
-    public String getModel(){
+    public String getModel() {
         return this.model;
     }
-    public void setModel(String model){
+
+    public void setModel(String model) {
         this.model = model;
     }
 
-    public String getCarrier(){
+    public String getCarrier() {
         return this.carrier;
     }
-    public void setCarrier(String carrier){
+
+    public void setCarrier(String carrier) {
         this.carrier = carrier;
     }
 
-    public String getPhoneNumber(){
+    public String getPhoneNumber() {
         return this.phoneNumber;
     }
-    public void setPhoneNumber(String phoneNumber){
+
+    public void setPhoneNumber(String phoneNumber) {
         this.phoneNumber = phoneNumber;
     }
 
-    public String getOwner(){
+    public String getOwner() {
         return this.owner;
     }
-    public void setOwner(String owner){
+
+    public void setOwner(String owner) {
         this.owner = owner;
     }
+
     //Overload Ex3
-    public CellPhone(int serialNumber, String model, String carrier, String phoneNumber, String owner){
+    public CellPhone(int serialNumber, String model, String carrier, String phoneNumber, String owner) {
         /*setSerialNumber(serialNumber);
         setModel(model);
         setCarrier(carrier);
@@ -55,21 +61,25 @@ public class CellPhone {
     }
 
     //Why use the get/set when you can just use scanned vars + print???
-    public void printAll(int serialNumber, String model, String carrier, String phoneNumber, String owner){
+    public void printAll(int serialNumber, String model, String carrier, String phoneNumber, String owner) {
         System.out.println("Serial Number: " + serialNumber + "\n" +
                 "Model: " + model + "\n" +
                 "Carrier: " + carrier + "\n" +
                 "Phone Number: " + phoneNumber + "\n" +
-                "Owner Name: " + owner );
+                "Owner Name: " + owner);
     }
 
     //public void dial(CellPhone cellphone){
-    public  void dial(String owner, String phoneNumber){
+    public void dial(String owner, String phoneNumber) {
+        System.out.println("--------------------");
         System.out.println(owner + "'s phone is calling you: " + phoneNumber);// wanted to try to add some () like (480)
         //tho many not necessary since user may input that
         //unless there's an if else to check?
         //then maybe use split? refer to BGs Ex for refresher
     }
 
+    public void dial(CellPhone phone) {
+        System.out.println(getOwner() + " is calling you! Phone Number: " + phone.getPhoneNumber());
+    }
 
 }
--- src/main/java/com/pluarlsight/CellPhoneService.java
+++ src/main/java/com/pluarlsight/CellPhoneService.java
@@ -1,6 +1,5 @@
 package com.pluarlsight;
 
-import java.util.Random;
 import java.util.Scanner;
 
 public class CellPhoneService {
@@ -11,6 +10,8 @@ public class CellPhoneService {
     static String carrier;
     static String phoneNumber;
     static String owner;
+    static int displayCall = 0;
+    static int myCount = 0;
 
     public static void main(String[] args) {
         /*What is the serial number? 2597153
@@ -21,31 +22,44 @@ public class CellPhoneService {
         */
         System.out.println("What is the serial number?");
         serialNumber = scan.nextInt();
-        System.out.println("What model is the phone?");
-        scan.nextLine(); //Necessary
-        model = scan.nextLine();
-        System.out.println("Who is the carrier?");
-        carrier = scan.nextLine();
-        System.out.println("What is the phone number?");
-        phoneNumber = scan.nextLine();
-        System.out.println("Who is the owner of the phone?");
-        owner = scan.nextLine();
+        if (serialNumber == 0) {
+            myVars();
+        } else {
+            System.out.println("What model is the phone?");
+            scan.nextLine(); //Necessary
+            model = scan.nextLine();
+            System.out.println("Who is the carrier?");
+            carrier = scan.nextLine();
+            System.out.println("What is the phone number?");
+            phoneNumber = scan.nextLine();
+            System.out.println("Who is the owner of the phone?");
+            owner = scan.nextLine();
+        }
 
         //Instantiate object
         CellPhone cellPhone1 = new CellPhone();
+        if (myCount == 1){
+            cellPhone1.setSerialNumber(2597153);
+            cellPhone1.setModel("iPhone 15 Pro Max");
+            cellPhone1.setCarrier("Verizon");
+            cellPhone1.setPhoneNumber("888-555-1234");
+            cellPhone1.setOwner("Sandra");
+        } else {
         //Setters from user
         cellPhone1.setSerialNumber(serialNumber);
         cellPhone1.setModel(model);
         cellPhone1.setCarrier(carrier);
         cellPhone1.setPhoneNumber(phoneNumber);
         cellPhone1.setOwner(owner);
+        }
+
         //Setters for Pre-defined user
         CellPhone cellPhone2 = new CellPhone();
-        cellPhone1.setSerialNumber(01234567);//maybe use long if irl
-        cellPhone1.setModel("Galaxy S12");
-        cellPhone1.setCarrier("Verison");
-        cellPhone1.setPhoneNumber("480-555-8888");
-        cellPhone1.setOwner("Danny");
+        cellPhone2.setSerialNumber(01234567);//maybe use long if irl
+        cellPhone2.setModel("Galaxy S12");
+        cellPhone2.setCarrier("Verison");
+        cellPhone2.setPhoneNumber("480-555-8888");
+        cellPhone2.setOwner("Danny");
         //Print getters
         /*System.out.println("Serial Number" + cellPhone1.getSerialNumber());
         System.out.println(cellPhone1.getModel());;
@@ -62,9 +76,17 @@ public class CellPhoneService {
 
         //Ex3
         CellPhone cellPhone3 = new CellPhone();
-        setVars(cellPhone3);
-        cellPhone3.dial(cellPhone1.getOwner(), cellPhone1.getPhoneNumber());
+        cellPhone3.setSerialNumber(4599548);//maybe use long if irl
+        cellPhone3.setModel("Google Pixel");
+        cellPhone3.setCarrier("Cricket");
+        cellPhone3.setPhoneNumber("900-000-1111");
+        cellPhone3.setOwner("Koolie");
+        //setVars(cellPhone3);
+        cellPhone3.dial(cellPhone3.getOwner(), cellPhone3.getPhoneNumber());
+        System.out.println();
         display(cellPhone3);
+        //Overloaded dial
+        cellPhone3.dial(cellPhone1);
 
 
 
@@ -80,17 +102,33 @@ public class CellPhoneService {
     }
     //In your Main class, add a static method called display() that accepts a CellPhone object as a parameter.  (see below)
     public static void display(CellPhone phone) {
-        phone.printAll(serialNumber, model, carrier, phoneNumber , owner);
+        displayCall++;
+        System.out.println("--------------------");
+        System.out.println(phone.getOwner() + "'s Info (Call # " + displayCall + ")");
+        //phone.printAll(serialNumber, model, carrier, phoneNumber , owner);
+        phone.printAll(phone.getSerialNumber(), phone.getModel(),phone.getCarrier(), phone.getPhoneNumber(), phone.getOwner());
     }
 
 
     //Ex3
-    public static void setVars (CellPhone phone){
+    //Legacy after printAll() was fixed by applying phone.get to display()
+    /*public static void setVars (CellPhone phone){
         phone.setSerialNumber(serialNumber);
         phone.setModel(model);
         phone.setCarrier(carrier);
         phone.setPhoneNumber(phoneNumber);
         phone.setOwner(owner);
+    }*/
+
+    public static void myVars(){
+        myCount ++;
+        //TIL Text Block!
+        System.out.println("""
+                Serial Number: 123456789
+                Model: Samsung Galaxy Note 9
+                Carrier: Mint
+                Phone Number: 602-555-7777
+                Owner Name: Brian Ve""");
     }
 
     //++ a tempVar everytime a method is called??? OR if newObject created ++ tempVar
