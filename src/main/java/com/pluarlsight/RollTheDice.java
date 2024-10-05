47e56ec424b22392be421a430ea87b7f8ec71d9c
BrianVegaSol <brivegasol@gmail.com>
2024-10-05 09:52:34 -0700

    RollTheDice Derp
    Get the switch to work properly for counting the dice
    But should've made the () result ops cx

--- src/main/java/com/pluarlsight/RollTheDice.java
+++ src/main/java/com/pluarlsight/RollTheDice.java
@@ -9,16 +9,20 @@ public class RollTheDice {
         int count2Rolls = 0;
         int count4Rolls = 0;
         int count6Rolls = 0;
+        int count7Rolls = 0;
 
         Dice dice = new Dice();
 
-        for (i = 1; i < 21; i++) {
+        for (i = 1; i < 5; i++) {
             roll1 = Dice.roll();
             roll2 = Dice.roll();
             result = roll1 + roll2;
-            System.out.println("Round " + i + ": " + Dice.roll());
-            //System.out.println("--------------------------");
-            if (roll1 == 2 || roll2 == 2) {
+            System.out.println("Round " + i + " \n\n" +
+                    "Die 1: " + roll1 + "\n" +
+                    "Die 2: " + roll2 + "\n" +
+                    "Result: " + result);
+            System.out.println("--------------------------");
+            /*if (roll1 == 2 || roll2 == 2) {
                 count2Rolls++;
             }
             if (Dice.roll() == 4) {
@@ -26,12 +30,43 @@ public class RollTheDice {
             }
             if (Dice.roll() == 6) {
                 count6Rolls++;
+            }*/
+            //Never forget the breaks lol
+            //switch (Dice.roll()){ Getting weird results
+            switch (roll1){
+                case 2:
+                    count2Rolls ++;
+                    break;
+                case 4:
+                    count4Rolls ++;
+                    break;
+                case 6:
+                    count6Rolls ++;
+                    break;
+                case 7:
+                    count7Rolls ++;
+                    break;
+            }
+            switch (roll2){
+                case 2:
+                    count2Rolls ++;
+                    break;
+                case 4:
+                    count4Rolls ++;
+                    break;
+                case 6:
+                    count6Rolls ++;
+                    break;
+                case 7:
+                    count7Rolls ++;
+                    break;
             }
         }
         //Tallys
-        System.out.println("--------------------------");
+        //System.out.println("--------------------------");
         System.out.println("#2 Rolled: " + count2Rolls + " times");
         System.out.println("#4 Rolled: " + count4Rolls + " times");
         System.out.println("#6 Rolled: " + count6Rolls + " times");
+        System.out.println("#7 Rolled: " + count7Rolls + " times");
     }
 }
